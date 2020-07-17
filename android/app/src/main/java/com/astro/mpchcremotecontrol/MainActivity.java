311af4c9f03d946391abeb5d75c83ebd9f2d8843
Alejandro Leal <aleaberm12@gmail.com>
2020-07-17 14:24:26 +0200

    Proyecto renombrado

--- android/app/src/main/java/com/astro/mpchcremotecontrol/MainActivity.java
+++ android/app/src/main/java/com/astro/mpchcremotecontrol/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rnb;
+package com.astro.mpchcremotecontrol;
 
 import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
@@ -19,7 +19,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "rnb";
+    return "MPCHCRemoteControl";
   }
 
   @Override
--- android/app/src/main/java/com/astro/mpchcremotecontrol/MainApplication.java
+++ android/app/src/main/java/com/astro/mpchcremotecontrol/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rnb;
+package com.astro.mpchcremotecontrol;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.rnb.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.astro.mpchcremotecontrol.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
