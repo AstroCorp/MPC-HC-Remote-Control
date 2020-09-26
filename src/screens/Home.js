54c1592db6df93f2911d054db0139fda4f43340e
Alejandro Leal <aleaberm12@gmail.com>
2020-09-27 00:41:51 +0200

    Info del vídeo actual en MPC-HC

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,37 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import { Text, Button, ImageBackground, View, StyleSheet } from 'react-native';
 import Header from '../components/Header';
 import SafeAreaView from 'react-native-safe-area-view';
+import { WebView } from 'react-native-webview';
 import axios from "axios";
 import { sendCommand } from '../store/actions';
 import { volumeUp, volumeDown } from '../utils/commands';
 import { getVariables } from '../utils/variables';
 
 const Home = (props) => {
+    const webview = useRef(null);
     const [mpcInfo, setMpcInfo] = useState(null);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            axios.get('http://' + props.ip + ':' + props.port + "/variables.html").then((res) => {
-                setMpcInfo(getVariables(res.data));
-            }).catch((err) => {
-                setMpcInfo(null);
-            });
+    const INJECTED_JAVASCRIPT =  `
+        window.check = setInterval(() => {
+            if(document.querySelector(".page-variables") !== null) {
+                window.ReactNativeWebView.postMessage(document.querySelector(".page-variables").innerHTML);
+                clearInterval(window.check);
+            }
         }, 250);
-
-        return () => clearInterval(interval);
-    }, []);
+    `;
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
+            <WebView 
+                ref={webview}
+                containerStyle={{ display: 'none' }}
+                source={{ 
+                    uri: 'http://' + props.ip + ':' + props.port + '/variables.html'
+                }}
+                javaScriptEnabled={true}
+                injectedJavaScript={INJECTED_JAVASCRIPT}
+                onMessage={event => {
+                    setMpcInfo(getVariables(event.nativeEvent.data));
+                    webview.current.reload();
+                }}
+                onError={() => {
+                    webview.current.reload();
+                }}
+            />
+
             <Header title={props.ip + ' : ' + props.port} navigation={props.navigation} />
             {/*
             <ImageBackground source={image} style={styles.image}>
                 <View>
                     <Text style={styles.text}>file.mp4</Text>
                 </View>
-            </ImageBackground>*/}
+            </ImageBackground>
+            */}
 
             <Text>{JSON.stringify(mpcInfo)}</Text>
 
